3e8d1f2d8da663fea9be5a66dd3766988669158b
AKASH-ROYAL <rosegold060402@gmail.com>
2023-10-27 10:43:12 +0530

    first commit

---
+++ android/app/src/main/java/com/example/why0823/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.why0823;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/new_main.dart
@@ -0,0 +1,48 @@
+
+
+
+void main() {
+  int number = 52;
+  int result = factorial(number);
+  print("The factorial of $number is $result");
+
+  int numberOfTerms = 10;
+  List<int> fibonacciSeries = generateFibonacci(numberOfTerms);
+
+  print(
+      "The first $numberOfTerms terms of the Fibonacci sequence are: $fibonacciSeries");
+}
+
+
+
+List<int> generateFibonacci(int terms) {
+  List<int> fibonacciSequence = [];
+
+  if (terms < 1) {
+    return fibonacciSequence;
+  }
+
+  int a = 0, b = 1;
+  fibonacciSequence.add(a);
+  if (terms > 1) {
+    fibonacciSequence.add(b);
+  }
+
+  for (int i = 2; i < terms; i++) {
+    int next = a + b;
+    fibonacciSequence.add(next);
+    a = b;
+    b = next;
+  }
+
+  return fibonacciSequence;
+}
+
+
+int factorial(int n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
